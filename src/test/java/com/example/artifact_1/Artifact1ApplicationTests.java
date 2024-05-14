926d1439168b3099986b686666542313cb120250
omerdor001 <102467386+omerdor001@users.noreply.github.com>
2024-05-14 19:18:06 +0300

    create spring-boot project

---
+++ src/test/java/com/example/artifact_1/Artifact1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.artifact_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Artifact1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
