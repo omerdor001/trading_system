06834f1091e36077bf0568201b513ef168d11846
lana <lanafa@post.bgu.ac.il>
2024-05-20 16:26:07 +0300

    added category and store classes

--- src/main/java/com/example/trading_system/domain/stores/Category.java
+++ src/main/java/com/example/trading_system/domain/stores/Category.java
@@ -3,5 +3,35 @@ package com.example.trading_system.domain.stores;
 public class Category {
     private String categoryName;
     private String categoryDescription;
+    private List<Product> products = new ArrayList<>();
+    public  Category(String categoryName,String categoryDescription){
+        this.categoryName = categoryName;
+        this.categoryDescription = categoryDescription;
+        this.products = new ArrayList<>();
+    }
+    public String getCategoryName() {
+        return categoryName;
+    }
+    public void setCategoryName(String categoryDescription) {
+        this.categoryName = categoryName;
+    }
 
+    public String getCategoryDescription() {
+        return categoryDescription;
+    }
+
+    public void setCategoryDescription(String categoryDescription) {
+        this.categoryDescription = categoryDescription;
+    }
+    public List<Product> getProducts() {
+        return products;
+    }
+    public void addProduct(Product product) {
+        products.add(product);
+        product.setCategory(categoryName);
+    }
+    public void removeProduct(Product product) {
+        products.remove(product);
+    }
+    
 }
