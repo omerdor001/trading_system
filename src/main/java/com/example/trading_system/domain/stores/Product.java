f44c69556753b8f43e3bb3a7e301c916f4da1081
Yaron Erikh <yaronarik@gmail.com>
2024-05-20 23:15:40 +0300

    Solved conflict (Product)

--- src/main/java/com/example/trading_system/domain/stores/Product.java
+++ src/main/java/com/example/trading_system/domain/stores/Product.java
@@ -26,53 +26,53 @@ public class Product {
         this.category_id = "";
     }
 
-//    public int getProduct_id() {
-//        return product_id;
-//    }
-//    public void setProduct_id(int product_id) {
-//        this.product_id = product_id;
-//    }
-//    public String getProduct_name() {
-//        return product_name;
-//    }
-//    public void setProduct_name(String product_name) {
-//        this.product_name = product_name;
-//    }
-//    public String getProduct_description() {
-//        return product_description;
-//    }
-//    public void setProduct_description(String product_description) {
-//        this.product_description = product_description;
-//    }
-//    public int getProduct_price() {
-//        return product_price;
-//    }
-//    public void setProduct_price(int product_price) {
-//        this.product_price = product_price;
-//    }
-//    public int getProduct_quantity() {
-//        return product_quantity;
-//    }
-//    public void setProduct_quantity(int product_quantity) {
-//        this.product_quantity = product_quantity;
-//    }
-//    public int getRating() {
-//        return rating;
-//    }
-//    public void setRating(int rating) {
-//        this.rating = rating;
-//    }
-//    public String getStore_id() {
-//        return store_id;
-//    }
-//    public void setStore_id(String store_id) {
-//        this.store_id = store_id;
-//    }
-//    public String getCategory_id() {
-//        return category_id;
-//    }
-//    public void setCategory_id(String category_id) {
-//        this.category_id = category_id;
-//    }
+    public int getProduct_id() {
+        return product_id;
+    }
+    public void setProduct_id(int product_id) {
+        this.product_id = product_id;
+    }
+    public String getProduct_name() {
+        return product_name;
+    }
+    public void setProduct_name(String product_name) {
+        this.product_name = product_name;
+    }
+    public String getProduct_description() {
+        return product_description;
+    }
+    public void setProduct_description(String product_description) {
+        this.product_description = product_description;
+    }
+    public int getProduct_price() {
+        return product_price;
+    }
+    public void setProduct_price(int product_price) {
+        this.product_price = product_price;
+    }
+    public int getProduct_quantity() {
+        return product_quantity;
+    }
+    public void setProduct_quantity(int product_quantity) {
+        this.product_quantity = product_quantity;
+    }
+    public int getRating() {
+        return rating;
+    }
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+    public String getStore_id() {
+        return store_id;
+    }
+    public void setStore_id(String store_id) {
+        this.store_id = store_id;
+    }
+    public String getCategory_id() {
+        return category_id;
+    }
+    public void setCategory_id(String category_id) {
+        this.category_id = category_id;
+    }
 
 }
