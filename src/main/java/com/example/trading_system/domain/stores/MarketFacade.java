afda50622cbb7a920ce3ecd486106a940b4544e0
lana <lanafa@post.bgu.ac.il>
2024-05-22 01:49:42 +0300

    added search products in stores function to the domain

--- src/main/java/com/example/trading_system/domain/stores/MarketFacade.java
+++ src/main/java/com/example/trading_system/domain/stores/MarketFacade.java
@@ -3,7 +3,14 @@ package com.example.trading_system.domain.stores;
 public interface MarketFacade {
 
     String getAllStores();
+
     String getStoreProducts(String store_name);
+
     String getProductInfo(String store_name, int product_id);
 
+    String searchNameInStores(String name, Double minPrice, Double maxPrice, Double minRating, Category category);
+
+    String searchCategoryInStores(Category category, Double minPrice, Double maxPrice, Double minRating);
+
+    String searchKeywordsInStores(String keyWords, Double minPrice, Double maxPrice, Double minRating, Category category);
 }
--- src/main/java/com/example/trading_system/domain/stores/MarketFacadeImp.java
+++ src/main/java/com/example/trading_system/domain/stores/MarketFacadeImp.java
@@ -2,15 +2,15 @@ package com.example.trading_system.domain.stores;
 
 import java.util.HashMap;
 
-public class MarketFacadeImp {
-    private HashMap<String,Store> stores;
+public class MarketFacadeImp implements MarketFacade {
+    private HashMap<String, Store> stores;
 
 
     public MarketFacadeImp() {
         stores = new HashMap<>();
     }
 
-    public String getAllStores(){
+    public String getAllStores() {
         StringBuilder sb = new StringBuilder();
         sb.append("{");
         sb.append(", \"stores\":[");
@@ -23,11 +23,37 @@ public class MarketFacadeImp {
         sb.append("}");
         return sb.toString();
     }
-    public String getStoreProducts(String store_name){
+
+    public String getStoreProducts(String store_name) {
         return stores.get(store_name).toString();
     }
-    public String getProductInfo(String store_name, int product_id){
+
+    public String getProductInfo(String store_name, int product_id) {
         return stores.get(store_name).getProduct(product_id).toString();
+    }
+
+    public String searchNameInStores(String name, Double minPrice, Double maxPrice, Double minRating, Category category) {
+        StringBuilder sb = new StringBuilder();
+        for (Store store : stores.values()) {
+            sb.append(store.searchName(name, minPrice, maxPrice, minRating, category).toString());
+        }
+        return sb.toString();
+    }
+
+    public String searchCategoryInStores(Category category, Double minPrice, Double maxPrice, Double minRating) {
+        StringBuilder sb = new StringBuilder();
+        for (Store store : stores.values()) {
+            sb.append(store.searchCategory(category, minPrice, maxPrice, minRating).toString());
+        }
+        return sb.toString();
+    }
+
+    public String searchKeywordsInStores(String keyWords, Double minPrice, Double maxPrice, Double minRating, Category category) {
+        StringBuilder sb = new StringBuilder();
+        for (Store store : stores.values()) {
+            sb.append(store.searchKeywords(keyWords, minPrice, maxPrice, minRating, category).toString());
+        }
+        return sb.toString();
     }
 
 }
