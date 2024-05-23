6d0b2428290c2dac535b52cd07bfc294d0446b24
lana <lanafa@post.bgu.ac.il>
2024-05-23 17:00:57 +0300

    added opening store function

--- src/main/java/com/example/trading_system/service/UserService.java
+++ src/main/java/com/example/trading_system/service/UserService.java
@@ -1,8 +1,13 @@
 package com.example.trading_system.service;
 
+import com.example.trading_system.domain.stores.StorePolicy;
+
 import java.time.LocalDate;
 
 public interface UserService {
 
     boolean registration(int id, String username, String password, LocalDate birthdate);
+    boolean addToCart(int id,int productId,String storeName,int quantity);
+    boolean openStore(String username,String storeName,String description , StorePolicy policy);
+
 }
