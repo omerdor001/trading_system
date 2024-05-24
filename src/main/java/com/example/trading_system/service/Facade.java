e8ada141c247b7b2704470e7d00cf7780b32605c
omerdor001 <omerdo@post.bgu.ac.il>
2024-05-24 16:44:25 +0300

    Use Case Stock Management - continue

--- src/main/java/com/example/trading_system/service/Facade.java
+++ src/main/java/com/example/trading_system/service/Facade.java
@@ -3,31 +3,24 @@ package com.example.trading_system.service;
 import com.example.trading_system.domain.externalservices.Service;
 import com.example.trading_system.domain.externalservices.ServiceFacade;
 import com.example.trading_system.domain.externalservices.ServiceFacadeImp;
-import com.example.trading_system.domain.stores.Category;
-import com.example.trading_system.domain.stores.MarketFacade;
 import com.example.trading_system.domain.users.UserFacade;
 import com.example.trading_system.domain.users.UserFacadeImp;
-import org.springframework.http.ResponseEntity;
 
 import javax.management.InstanceAlreadyExistsException;
-import java.time.LocalDate;
-import java.util.List;
 
 public class Facade {
     public ServiceFacade serviceFacade;
     public UserFacade userFacade;
-    public MarketFacade marketFacade;
 
     public ExternalServices externalServices;
     public UserService userService;
-    public MarketService marketService;
 
     public Facade(){
         serviceFacade=new ServiceFacadeImp();
         userFacade=new UserFacadeImp();
+
         externalServices=new ExternalServicesImp(serviceFacade);
         userService=new UserServiceImp(userFacade);
-        marketService=new MarketServiceImp(marketFacade);
     }
 
     public boolean addService(Service service) throws InstanceAlreadyExistsException {
@@ -48,34 +41,6 @@ public class Facade {
 
     public boolean makeDelivery(String serviceName,String address){
         return serviceFacade.makeDelivery(serviceName,address);
-    }
-
-    public ResponseEntity<String> addProduct(String username, int product_id, String store_name, String product_name, String product_description,
-                                             double product_price, int product_quantity, double rating, Category category, List<String> keyWords){
-        return marketService.addProduct(username,product_id,store_name,product_name,product_description,product_price,product_quantity,rating,category,keyWords);
-    }
-
-    public ResponseEntity<String> removeProduct(String username, String store_name, int product_id){
-        return marketService.removeProduct(username,store_name,product_id);
-    }
-
-    public ResponseEntity<String> setProduct_name(String username,String store_name_id,int productId,String product_name){
-        return marketService.setProduct_name(username,store_name_id,productId,product_name);
-    }
-    public ResponseEntity<String> setProduct_description(String username,String store_name_id,int productId,String product_description){
-        return marketService.setProduct_description(username,store_name_id,productId,product_description);
-    }
-    public ResponseEntity<String> setProduct_price(String username,String store_name_id,int productId,int product_price){
-        return marketService.setProduct_price(username,store_name_id,productId,product_price);
-    }
-    public ResponseEntity<String> setProduct_quantity(String username,String store_name_id,int productId,int product_quantity){
-        return marketService.setProduct_quantity(username,store_name_id,productId,product_quantity);
-    }
-    public ResponseEntity<String> setRating(String username,String store_name_id,int productId,int rating){
-        return marketService.setRating(username,store_name_id,productId,rating);
-    }
-    public ResponseEntity<String> setCategory(String username,String store_name_id,int productId,Category category){
-        return marketService.setCategory(username,store_name_id,productId,category);
     }
 
 }
