97d3f81112e10c21b6fd370eb7671cf6a7eaea73
lana <lanafa@post.bgu.ac.il>
2024-05-26 20:04:29 +0300

    added payment functions .

--- src/main/java/com/example/trading_system/domain/users/Cart.java
+++ src/main/java/com/example/trading_system/domain/users/Cart.java
@@ -1,34 +1,46 @@
 package com.example.trading_system.domain.users;
 
+import com.example.trading_system.domain.stores.Product;
+
 import java.util.HashMap;
+import java.util.Map;
 
 public class Cart {
     private HashMap<String, ShoppingBag> shoppingBags;
+
     public Cart() {
         shoppingBags = new HashMap<>();
     }
+
     public HashMap<String, ShoppingBag> getShoppingBags() {
         return shoppingBags;
     }
-    public void addShoppingBag(ShoppingBag shoppingBag,String storeId) {
+
+    public void addShoppingBag(ShoppingBag shoppingBag, String storeId) {
         shoppingBags.put(storeId, shoppingBag);
     }
-    public void addProductToCart(int productId, int quantity,String storeId) {
+
+    public synchronized void addProductToCart(int productId, int quantity, String storeId) {
         ShoppingBag shoppingBag = shoppingBags.get(storeId);
         shoppingBag.addProduct(productId, quantity);
     }
-    public void removeProductFromCart(int productId, int quantity,String storeId) {
+
+    public synchronized void removeProductFromCart(int productId, int quantity, String storeId) {
         ShoppingBag shoppingBag = shoppingBags.get(storeId);
-        shoppingBag.removeProduct(productId,quantity);
+        shoppingBag.removeProduct(productId, quantity);
     }
-    public void addShoppingBagToCart(ShoppingBag shoppingBag,String storeId) {
+
+    public void addShoppingBagToCart(ShoppingBag shoppingBag, String storeId) {
         shoppingBags.put(storeId, shoppingBag);
     }
-    public void removeShoppingBagFromCart(ShoppingBag shoppingBag,String storeId) {
+
+    public void removeShoppingBagFromCart(ShoppingBag shoppingBag, String storeId) {
         shoppingBags.remove(storeId);
     }
 
     public void saveCart() {
         //TODO when connecting to database.
     }
+
+
 }
