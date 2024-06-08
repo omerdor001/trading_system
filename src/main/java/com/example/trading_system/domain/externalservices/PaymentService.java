0d0a4eb9fd064a654e49376df8ccd7ab019e17f1
omerdor001 <omerdo@post.bgu.ac.il>
2024-06-08 09:08:18 +0300

    Removing service name from External Services

--- src/main/java/com/example/trading_system/domain/externalservices/PaymentService.java
+++ src/main/java/com/example/trading_system/domain/externalservices/PaymentService.java
@@ -1,7 +1,6 @@
 package com.example.trading_system.domain.externalservices;
 
 public interface PaymentService{
-    String getName();
     int makePayment(double amount);
     void cancelPayment(int paymentId);
     boolean connect();
