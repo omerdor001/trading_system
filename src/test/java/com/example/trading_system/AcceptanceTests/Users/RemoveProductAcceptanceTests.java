305d067e0f266d7897197cd4a0f8654e9b8fc0b1
omerdor001 <omerdo@post.bgu.ac.il>
2024-06-21 18:46:43 +0300

    Fixing

--- src/test/java/com/example/trading_system/AcceptanceTests/Users/RemoveProductAcceptanceTests.java
+++ src/test/java/com/example/trading_system/AcceptanceTests/Users/RemoveProductAcceptanceTests.java
@@ -45,7 +45,7 @@ public class RemoveProductAcceptanceTests {
         storeRepository= StoreMemoryRepository.getInstance();  //May be change later
         tradingSystemImp = TradingSystemImp.getInstance(mock(PaymentService.class),mock(DeliveryService.class),userRepository,storeRepository);
         if(tradingSystemImp==null){
-            logger.info("1");
+            logger.info("10");
         }
         tradingSystemImp.register("admin", password, LocalDate.now());
         tradingSystemImp.openSystem(storeRepository);
