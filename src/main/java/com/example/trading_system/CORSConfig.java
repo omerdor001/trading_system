ec53d53fd99d5611201c38dbc3c02f80ac00de81
yaron <yaronarik@gmail.com>
2024-07-03 22:51:11 +0300

    Configuration Spring CORSConfig

---
+++ src/main/java/com/example/trading_system/CORSConfig.java
@@ -0,0 +1,16 @@
+package com.example.trading_system;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class CORSConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedOrigins("http://localhost:8080"); // This is only for dev.
+    }
+}
