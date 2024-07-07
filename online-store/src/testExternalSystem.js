73385d9bee040a31a8c11a9946886851ffa801a6
yaron <yaronarik@gmail.com>
2024-07-07 21:12:39 +0300

    ExternalSystemService.js

--- online-store/src/testExternalSystem.js
+++ online-store/src/testExternalSystem.js
@@ -6,20 +6,42 @@ export async function testService() {
     try {
         const handshakeResponse = await service.handshake();
         console.log('Handshake Response:', handshakeResponse);
+    } catch (error) {
+        console.error('Service Error:', error);
+    }
+    try {
+
 
         const payResponse = await service.pay("1000", "USD", "2222333344445555", "4", "2021", "Israel Israelovice", "262", "20444444");
         console.log('Payment Response:', payResponse);
+    } catch (error) {
+        console.error('Service Error:', error);
+    }
+    try {
+
 
         const cancelPayResponse = await service.cancelPay("20123");
         console.log('Cancel Payment Response:', cancelPayResponse);
 
+    } catch (error) {
+        console.error('Service Error:', error);
+    }
+    try {
+
+
         const supplyResponse = await service.supply("Israel Israelovice", "Rager Blvd 12", "Beer Sheva", "Israel", "8458527");
         console.log('Supply Response:', supplyResponse);
+    } catch (error) {
+        console.error('Service Error:', error);
+    }
+    try {
+
 
         const cancelSupplyResponse = await service.cancelSupply("30525");
         console.log('Cancel Supply Response:', cancelSupplyResponse);
 
-    } catch (error) {
+    } catch
+        (error) {
         console.error('Service Error:', error);
     }
 }
