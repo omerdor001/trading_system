e5116d9bced7bf6233e2d710b306f1dfdf780ac6
omerdor001 <omerdo@post.bgu.ac.il>
2024-07-16 22:22:31 +0300

    Initialize starting

--- src/main/java/com/example/trading_system/TradingSystemApplication.java
+++ src/main/java/com/example/trading_system/TradingSystemApplication.java
@@ -1,13 +1,13 @@
 package com.example.trading_system;
 
 import com.example.trading_system.service.TradingSystemImp;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -23,9 +23,9 @@ public class TradingSystemApplication {
         SpringApplication.run(TradingSystemApplication.class, args);
     }
 
-    @Bean
+    @PostConstruct
     public void initializeSystem() {
-        initializationService.initializeSystem();
+        // initializationService.initializeSystem();
     }
 }
 
--- src/main/java/com/example/trading_system/TradingSystemInitializer.java
+++ src/main/java/com/example/trading_system/TradingSystemInitializer.java
@@ -1,11 +1,15 @@
 package com.example.trading_system;
-
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -13,10 +17,12 @@ import java.util.Map;
 import static com.example.trading_system.service.Security.generateToken;
 
 public class TradingSystemInitializer {
+    @Autowired
+    private RestTemplate restTemplate;
+    private Map<String, String> tokens=new HashMap<>();
     @PostConstruct
     public void initializeSystem() {
-        Map<String, String> tokens=new HashMap<>();
-
+        tokens=new HashMap<>();
         try {
             InputStream inputStream = getClass().getResourceAsStream("/initialState.json");
             ObjectMapper objectMapper = new ObjectMapper();
@@ -50,12 +56,89 @@ public class TradingSystemInitializer {
     private void executeUseCase(String useCaseName, Map<String, Object> arguments) {
         switch (useCaseName) {
             case "register":
-                // Example: userService.register(arguments);
+                LocalDate birthday = (LocalDate) arguments.get("birthday");
+                MultiValueMap<String, String> paramsRegister = new LinkedMultiValueMap<>();
+                paramsRegister.add("username", (String) arguments.get("username"));
+                paramsRegister.add("password", (String) arguments.get("password"));
+                paramsRegister.add("birthday", birthday.toString());
+                restTemplate.getForEntity("/api/trading/register", String.class, paramsRegister);
                 break;
             case "openSystem":
-                // Example: systemService.openSystem(arguments);
+                restTemplate.postForEntity("/api/trading/openSystem", null, String.class);
                 break;
-            // Add cases for other use cases...
+            case "enter":
+                restTemplate.postForEntity("/api/trading/enter", null, String.class);
+                break;
+            case "login0":
+                MultiValueMap<String, String> paramsLogin0 = new LinkedMultiValueMap<>();
+                paramsLogin0.add("token", tokens.get("v0"));
+                paramsLogin0.add("usernameV", (String) arguments.get("usernameV"));
+                paramsLogin0.add("username", (String) arguments.get("username"));
+                paramsLogin0.add("password", (String) arguments.get("password"));
+                restTemplate.getForEntity("/api/trading/login", String.class, paramsLogin0);
+                break;
+            case "login2":
+                MultiValueMap<String, String> paramsLogin2 = new LinkedMultiValueMap<>();
+                paramsLogin2.add("token", tokens.get("v2"));
+                paramsLogin2.add("usernameV", (String) arguments.get("usernameV"));
+                paramsLogin2.add("username", (String) arguments.get("username"));
+                paramsLogin2.add("password", (String) arguments.get("password"));
+                restTemplate.getForEntity("/api/trading/login", String.class, paramsLogin2);
+                break;
+            case "login4":
+                MultiValueMap<String, String> paramsLogin4 = new LinkedMultiValueMap<>();
+                paramsLogin4.add("token", tokens.get("v4"));
+                paramsLogin4.add("usernameV", (String) arguments.get("usernameV"));
+                paramsLogin4.add("username", (String) arguments.get("username"));
+                paramsLogin4.add("password", (String) arguments.get("password"));
+                restTemplate.getForEntity("/api/trading/login", String.class, paramsLogin4);
+                break;
+            case "login7":
+                MultiValueMap<String, String> paramsLogin7 = new LinkedMultiValueMap<>();
+                paramsLogin7.add("token", tokens.get("v7"));
+                paramsLogin7.add("usernameV", (String) arguments.get("usernameV"));
+                paramsLogin7.add("username", (String) arguments.get("username"));
+                paramsLogin7.add("password", (String) arguments.get("password"));
+                restTemplate.getForEntity("/api/trading/login", String.class, paramsLogin7);
+                break;
+            case "openStore":
+                MultiValueMap<String, String> paramsOpenStore = new LinkedMultiValueMap<>();
+                paramsOpenStore.add("username", (String) arguments.get("username"));
+                paramsOpenStore.add("token", tokens.get("john_doe"));
+                paramsOpenStore.add("storeName", (String) arguments.get("storeName"));
+                paramsOpenStore.add("description", (String) arguments.get("description"));
+                restTemplate.getForEntity("/api/trading/create-store", String.class, paramsOpenStore);
+                break;
+            case "suggestOwner":
+                MultiValueMap<String, String> paramsSuggestOwner = new LinkedMultiValueMap<>();
+                paramsSuggestOwner.add("appoint", (String) arguments.get("appoint"));
+                paramsSuggestOwner.add("token", tokens.get("john_doe"));
+                paramsSuggestOwner.add("newOwner", (String) arguments.get("newOwner"));
+                paramsSuggestOwner.add("storeName", (String) arguments.get("storeName"));
+                restTemplate.getForEntity("/api/trading/suggestOwner", String.class, paramsSuggestOwner);
+                break;
+            case "suggestManage":
+                MultiValueMap<String, String> paramsSuggestManager = new LinkedMultiValueMap<>();
+                paramsSuggestManager.add("appoint", (String) arguments.get("appoint"));
+                paramsSuggestManager.add("token", tokens.get("john_doe"));
+                paramsSuggestManager.add("newManager", (String) arguments.get("newManager"));
+                paramsSuggestManager.add("storeName", (String) arguments.get("storeName"));
+                restTemplate.getForEntity("/api/trading/suggestManage", String.class, paramsSuggestManager);
+                break;
+            case "logout":
+                break;
+            case "exit":
+                break;
+            case "approveOwner":
+                break;
+            case "approveManage":
+                break;
+            case "addProduct":
+                break;
+            case "addToCart":
+                break;
+            case "approvePurchase":
+                break;
             default:
                 break;
         }
